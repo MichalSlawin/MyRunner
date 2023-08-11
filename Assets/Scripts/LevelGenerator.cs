82b6e7da6250103401e07ae336b96dcd97165cd8
Michal Slawinski <michal.slawin@gmail.com>
2023-08-11 14:33:59 +0200

    Generating simple obstacles

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -21,6 +21,7 @@ public class LevelGenerator : MonoBehaviour
         Vector3 position = _nextPart.transform.position;
         LevelPart newPart = Instantiate(_levelPartPrefab, 
             new Vector3(position.x, position.y, position.z + PART_Z_OFFSET), Quaternion.identity, transform);
+        newPart.Initialize();
 
         _currentPart = _nextPart;
         _nextPart = newPart;
--- Assets/Scripts/LevelPart.cs
+++ Assets/Scripts/LevelPart.cs
@@ -1,9 +1,38 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelPart : MonoBehaviour
 {
+    private const float MIN_OBSTACLES_DISTANCE = 5f;
+
+    [SerializeField] private List<GameObject> _obstaclesPrefabs;
+    [SerializeField] private Transform _obstacles;
+    [SerializeField] private Transform _obstaclesStart;
+    [SerializeField] private Transform _obstaclesEnd;
+
     public void Initialize()
     {
+        float startZ = _obstaclesStart.position.z;
+        float endZ = _obstaclesEnd.position.z;
 
+        for(float z = startZ; z <= endZ; z += MIN_OBSTACLES_DISTANCE)
+        {
+            TryInstantiateObstacle(-Game.LANE_OFFSET, z);
+            TryInstantiateObstacle(0, z);
+            TryInstantiateObstacle(Game.LANE_OFFSET, z);
+        }
+    }
+
+    private void TryInstantiateObstacle(float x, float z)
+    {
+        System.Random random = new System.Random();
+
+        if (random.Next(2) == 0)
+        {
+            GameObject prefab = _obstaclesPrefabs[random.Next(2)];
+            float y = prefab.transform.position.y + _obstacles.position.y;
+            Instantiate(prefab, new Vector3(x, y, z),
+                Quaternion.identity, _obstacles);
+        }
     }
 }
